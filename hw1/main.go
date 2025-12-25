bcccc654e1d1fea4e9ff2683170be3a7de7177dc
joeybreat <josephbreat@gmail.com>
2025-12-25 22:47:32 +0300

    Добавлена заготовка под функцию

--- hw1/main.go
+++ hw1/main.go
@@ -5,3 +5,7 @@ func main() {
 	const USDtoEUR float64 = 0.85
 	const EURtoRUB float64 = USDtoRUB / USDtoEUR
 }
+
+func convert(money int, currencyFrom string, currencyTo string) float64 {
+	return 0.0
+}
